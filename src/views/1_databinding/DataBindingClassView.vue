c85330e6c3c3235e45fa5f11273c517ba87e0573
lyae <lyaesley@gmail.com>
2022-10-16 22:00:33 +0900

    study

---
+++ src/views/1_databinding/DataBindingClassView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+      <div :class="{'text-red':hasError, active:isActive}">클래스 바인딩</div>
+      <div :class="class2">클래스 바인딩2</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DataBindingClassView',
+  data() {
+    return {
+      isActive: true,
+      hasError: false,
+      class2: ['active', 'text-red']
+    }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  background-color: greenyellow;
+  font-weight: bold;
+}
+
+.text-red {
+  color: red;
+}
+</style>
